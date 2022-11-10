02f1917d55d58f24375fd4d72644b280acc5c69d
md abdur rahim <rahim8760>
2022-11-10 23:27:28 +0600

    srvice

--- src/Pages/Services/Service.js
+++ src/Pages/Services/Service.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 const Service = ({allService}) => {
     const {photoUrl, Location, Description, rating, _id}=allService
     return (
